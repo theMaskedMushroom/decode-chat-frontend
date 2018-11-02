6df25adce7dd0469a28c4bda25d04b0e4d623585
Dude <dude@localhost.localdomain>
2018-11-02 16:25:42 -0400

    git ignore stuff

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -18,9 +18,15 @@ class Chat extends Component
         this.socket = null;
 
         // Bindings
-        //this.onMsgChange = this.onMsgChange.bind(this);
+        this.onMessageChange = this.onMessageChange.bind(this);
+        this.onSubmitMsg = this.onSubmitMsg.bind(this);
         this.onUserJoin = this.onUserJoin.bind(this);
         this.onUserLeft = this.onUserLeft.bind(this);
+    }
+
+    onMessageChange(evt)
+    {
+        this.setState({message: evt.target.value});
     }
 
     componentDidMount()
@@ -30,6 +36,7 @@ class Chat extends Component
 
         this.socket.on('userJoined', this.onUserJoin);
         this.socket.on('userLeft', this.onUserLeft);
+        this.socket.on('msg', this.onMessage);
 
         this.socket.connect();
     }
@@ -55,6 +62,11 @@ class Chat extends Component
         alert('user left: ' + data.serverMsg + "\nUsers" + data.users)
     }
 
+    onMessage(data)
+    {
+        alert('message => ' + data.msg);
+    }
+
     render()
     {
         return (
@@ -64,9 +76,9 @@ class Chat extends Component
                 <div> bob: hey </div>
             </div>
             <div className="botcontainer">
-                <form onSubmit={this.onSubmit}>
+                <form onSubmit={this.onSubmitMsg}>
                     <div className="chat">
-                        <input type="text"/>
+                        <input type="text" onChange={this.onMessageChange} value={this.state.message}/>
                         <input type="submit"/>
                     </div>
                 </form>
